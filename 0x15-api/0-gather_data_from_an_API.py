af1029fc1901c7ba33feead0439e4fa6dcff8e5e
benard Kiplangat <benardkiplangat97@gmail.com>
2024-01-29 20:26:29 +0300

    Gathering data from a given RESTapi

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+A script that returns a given employees information
+about his/her TODO list progress
+"""
+
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    employee = ""
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            total += 1
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
+
+    for i in tasks:
+        print("\t {}".format(i))
