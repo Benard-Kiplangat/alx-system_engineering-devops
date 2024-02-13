db087a0e403fe5c08a87901510b22b688eb9d1bc
benard Kiplangat <benardkiplangat97@gmail.com>
2024-02-13 23:37:31 +0300

    A script that prints the list of top 10 hot topics of a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that prints the titles of top ten hot posts ofa given subreddit"""
+
+from requests import get
+import requests
+import json
+
+
+def top_ten(subreddit):
+    """ A function that prints the titles of top ten hot posts"""
+
+    headers = {'user-agent': '/u/benardkiplangat API python for ALX Learning'}
+    client = requests.session()
+    client.headers = headers
+    count = 0
+    r = client.get('https://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                   allow_redirects=False)
+    if r.status_code != 200:
+        return (print("None"))
+    titles = r.json().get("data").get("children")
+    for title in titles[:10]:
+        print(title["data"]["title"])
+    return 0
