359f17a117ec125487d920b701ef68ebf86a227f
benard Kiplangat <benardkiplangat97@gmail.com>
2024-02-13 23:23:47 +0300

    Fixed pycodestyle errors: added another line before function declaration and fixed a long codeline

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,18 @@ from requests import get
 import requests
 import json
 
+
 def number_of_subscribers(subreddit):
+    """ A function that checks the number of subscribers of a given subredit"""
+
     headers = {'user-agent': '/u/benardkiplangat API python for ALX Learning'}
+
     client = requests.session()
     client.headers = headers
     count = 0
-    response = client.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), allow_redirects=False)
-    if response.status_code != 200:
+    r = client.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+                   allow_redirects=False)
+    if r.status_code != 200:
         return 0
-    data = response.json().get("data")
+    data = r.json().get("data")
     return (data.get("subscribers"))
