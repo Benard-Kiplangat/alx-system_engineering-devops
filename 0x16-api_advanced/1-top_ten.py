f3c76a8c7ffefff12165060e8de2171a7e25daca
benard Kiplangat <benardkiplangat97@gmail.com>
2024-02-16 00:35:17 +0300

    Removed unused imports

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,6 @@
 """A script that prints the titles of top ten hot posts ofa given subreddit"""
 
 import requests
-import json
 
 
 def top_ten(subreddit):
@@ -11,19 +10,9 @@ def top_ten(subreddit):
     header = {'user-agent': '/u/benardkiplangat API python for ALX Learning'}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     r = requests.get(url, allow_redirects=False, headers=header)
-    if r.status_code != 200:
+    if r.status_code == 404:
         return (print("None"))
     titles = r.json().get("data").get("children")
     for title in titles[:10]:
         print(title["data"]["title"])
     return 0
-
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        titles = top_ten(subreddit)
