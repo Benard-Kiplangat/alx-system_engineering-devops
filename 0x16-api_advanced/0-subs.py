482c224f928b9daa91f0ad35ca92bf32f0be2e81
benard Kiplangat <benardkiplangat97@gmail.com>
2024-02-16 00:31:31 +0300

    Fixed when to fail

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,16 @@
 """ A script that checks the number of subscribers of a given subreddit"""
 
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
     """ A function that checks the number of subscribers of a given subredit"""
 
-    header = {'user-agent': '/u/benardkiplangat API python for ALX Learning'}
+    header = {
+            'user-agent': '/u/benardkiplangat API python for ALX Learning'
+            }
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    r = requests.get(url, allow_redirects=False, headers=header)
+    r = requests.get(url, headers=header, allow_redirects=False)
     if r.status_code == 404:
         return 0
     data = r.json().get("data")
